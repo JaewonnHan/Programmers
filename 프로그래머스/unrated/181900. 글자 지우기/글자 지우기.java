ca40a6ef5df2de57e196b85875dd14f5d3861dd7
Jaewon <60962150+JaewonnHan@users.noreply.github.com>
2023-06-14 22:22:57 +0900

    [unrated] Title: 글자 지우기, Time: 15.02 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        String answer = "";
+        List<Integer> list = new ArrayList<>();
+        for(int a : indices){
+            list.add(a);
+        }
+        for(int i=0; i<my_string.length(); i++){
+            if(!(list.contains(i))){
+                answer += my_string.charAt(i);
+            }
+        }
+        
+        
+        
+        return answer;
+    }
+}
