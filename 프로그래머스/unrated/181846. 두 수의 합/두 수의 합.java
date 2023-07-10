f59368207adc03c05d0c3c7f698e80f481d62caf
Jaewon <60962150+JaewonnHan@users.noreply.github.com>
2023-07-10 13:56:20 +0900

    Delete 프로그래머스/unrated/181846. 두 수의 합 directory

--- 프로그래머스/unrated/181846. 두 수의 합/두 수의 합.java
+++
@@ -1,7 +0,0 @@
-class Solution {
-    public String solution(String a, String b) {
-        int number = Integer.parseInt(a) + Integer.parseInt(b);
-        String answer = "" + number;
-        return answer;
-    }
-}
