d028bde8cbfa895bcfee76d72b725e1f9a67ff33
Jaewon <60962150+JaewonnHan@users.noreply.github.com>
2024-01-11 12:24:43 +0900

    [Bronze IV] Title: 주사위 세개, Time: 204 ms, Memory: 17628 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -8,46 +8,50 @@ public class Main {
 		a = in.nextInt();
 		b = in.nextInt();
 		c = in.nextInt();
-
+ 
+		// 만약 모든 변수가 다른 경우
 		if (a != b && b != c && a != c) {
-			int max=0;
-			
+			int max;
+			// 만약 a > b 라면
 			if (a > b) {
-		
+				// c > a > b 라면
 				if (c > a) {
 					max = c;
 				} 
-				
+				// a > (b, c)
 				else {
 					max = a;
 				}
 			}
-		
-			else if(b>a) {
-				
+			// b > a 라면	
+			else {
+				// c > b > a 라면
 				if (c > b) {
 					max = c;
 				}
-			
+				// b > (a, c)
 				else {
 					max = b;
 				}
 			}
 			System.out.println(max * 100);
 		}
-		
-		else if(a==b && b==c && c==a) {
-            	System.out.println(10000 + a * 1000);
-        }
+		// 적어도 한 쌍 이상의 서로 같은 변수가 존재할 경우
 		else {
-				
+			// 3개의 변수가 모두 같은 경우
+			if (a == b && a == c) {
+				System.out.println(10000 + a * 1000);
+			}
+			else {
+				// a가 b혹은 c랑만 같은 경우
 				if(a == b || a == c) {
 					System.out.println(1000 + a * 100);
 				}
-				
+				// b가 c랑 같은 경우
 				else {
 					System.out.println(1000 + b * 100);
 				}
 			}
 		}
 	}
+}
