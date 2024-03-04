d0105aa8f92137e62da5cf5c4ea492c2ae930586
Jaewon <60962150+JaewonnHan@users.noreply.github.com>
2024-03-04 10:07:04 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 144 ms, Memory: 15848 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -2,13 +2,14 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException{
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
         int[] chess = {1,1,2,2,2,8};
         int[] number = new int[chess.length];
         
         for(int i=0; i<chess.length; i++){
-            number[i] = sc.nextInt();
+            number[i] = Integer.parseInt(st.nextToken());
         }
         for(int i=0; i<chess.length; i++){
             System.out.print(chess[i] - number[i]+" ");
