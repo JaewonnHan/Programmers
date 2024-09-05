5497558fbc1d432ebb5a79a67310306614c8c098
Jaewon <60962150+JaewonnHan@users.noreply.github.com>
2024-09-05 15:51:21 +0900

    [Bronze III] Title: 최소, 최대, Time: 1192 ms, Memory: 106228 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -4,15 +4,14 @@ public class Main{
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        
-        int index = 0;
-        int[] arr = new int[N];
-        while(st.hasMoreTokens()){
-            arr[index]=Integer.parseInt(st.nextToken());
-            index++;
+        StringTokenizer str = new StringTokenizer(br.readLine());
+        int[] array = new int[N];
+        int count = 0;
+        while(str.hasMoreTokens()){
+            array[count] = Integer.parseInt(str.nextToken());
+            count++;
         }
-        Arrays.sort(arr);
-        System.out.println(arr[0]+" "+arr[N-1]);
+        Arrays.sort(array);
+        System.out.print(array[0] + " " + array[N-1]);
     }
 }
