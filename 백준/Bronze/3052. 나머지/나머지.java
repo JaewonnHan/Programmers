b802d64ac21efab38c64762c242be80fa95f76cf
Jaewon <60962150+JaewonnHan@users.noreply.github.com>
2024-09-09 14:52:36 +0900

    [Bronze II] Title: 나머지, Time: 100 ms, Memory: 14064 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,14 +1,12 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 public class Main{
-    public static void main(String[] args)throws IOException{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        HashSet<Integer> h = new HashSet<Integer>();
-        
+        HashSet<Integer> hs = new HashSet<Integer>();
         for(int i=0; i<10; i++){
-            h.add(Integer.parseInt(br.readLine())%42);
+            hs.add(Integer.parseInt(br.readLine()) % 42);
         }
-        System.out.println(h.size());
+        System.out.println(hs.size());
     }
-       
 }
