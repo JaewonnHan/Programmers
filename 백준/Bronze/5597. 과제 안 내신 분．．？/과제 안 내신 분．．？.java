9b804a19af613bcf885d6d040368e2fd2bc90b54
Jaewon <60962150+JaewonnHan@users.noreply.github.com>
2024-09-09 14:44:15 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 100 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,15 +1,15 @@
+import java.io.*;
 import java.util.*;
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int[] students = new int[30];
-        for(int i=0; i<28;i++){
-            int num= sc.nextInt();
-            students[num-1]=1;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[]array = new int[31];
+        for(int i=1; i<=28; i++){
+            array[Integer.parseInt(br.readLine())] = 1;
         }
-        for(int i=0; i<30; i++){
-            if(students[i] == 0){
-                System.out.println(i+1);
+        for(int j=1; j<=30; j++){
+            if(array[j] == 0){
+                System.out.println(j);
             }
         }
     }
