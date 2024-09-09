fc8f54ca6aaa7c377c108d111d9e5325cc32589f
Jaewon <60962150+JaewonnHan@users.noreply.github.com>
2024-09-09 14:33:21 +0900

    [Bronze II] Title: 공 바꾸기, Time: 120 ms, Memory: 15928 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -1,32 +1,33 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 public class Main{
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N];
-        int temp;
-        for (int i=0; i<arr.length; i++){
-            arr[i] = i+1;
+        int[] array = new int[N];
+         int med = 0;
+        for(int i=0; i<N; i++){
+            array[i] = i+1;
         }
         for(int j=0; j<M; j++){
-            st = new StringTokenizer(br.readLine()," ");
-            int P = Integer.parseInt(st.nextToken());
-            int Q = Integer.parseInt(st.nextToken());
-            temp = arr[P-1];
-            arr[P-1] = arr[Q-1];
-            arr[Q-1] = temp;
-        }
-        for(int value : arr){
-            bw.write(value+" ");
+              
+               st = new StringTokenizer(br.readLine());
+               int P = Integer.parseInt(st.nextToken());
+               int Q = Integer.parseInt(st.nextToken());
+               med = array[P-1];
+               array[P-1] = array[Q-1];
+               array[Q-1] = med;
+            }
+        
+      
+        for(int s=0; s<array.length; s++){
+            bw.write(array[s] + " ");
         }
         br.close();
         bw.flush();
         bw.close();
-       
     }
 }
