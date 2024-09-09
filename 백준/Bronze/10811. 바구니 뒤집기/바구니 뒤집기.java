2a9666dffe7a5c92991afb53a42bd84a65218cbc
Jaewon <60962150+JaewonnHan@users.noreply.github.com>
2024-09-09 15:12:40 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 120 ms, Memory: 15984 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -1,33 +1,33 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 public class Main{
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int temp = 0;
-        int[] arr = new int[N];
-        for(int i=0; i<arr.length; i++){
-            arr[i] = i+1;
+        int[] array = new int[N+1];
+        for(int i=1; i<=N; i++){
+            array[i] = i;
         }
-        for(int i=0; i<M; i++){
-            st = new StringTokenizer(br.readLine()," ");
-            int a = Integer.parseInt(st.nextToken())-1;
-            int b = Integer.parseInt(st.nextToken())-1;
-            while(a<b){
-                temp = arr[a];
-                arr[a] = arr[b];
-                arr[b] = temp;
-                a++;
-                b--;
+        for(int j=0; j<M; j++){
+            int med = 0;
+            st = new StringTokenizer(br.readLine());
+            int left = Integer.parseInt(st.nextToken());
+            int right = Integer.parseInt(st.nextToken());
+            while(left < right){
+                med = array[left];
+                array[left] = array[right];
+                array[right] = med;
+                left ++;
+                right--;
             }
-                
-            
+           
         }
-        br.close();
-        for(int value : arr){
-            System.out.print(value + " ");
+        for(int s=1; s<array.length; s++){
+            System.out.print(array[s] + " ");
         }
+        
+        
     }
 }
