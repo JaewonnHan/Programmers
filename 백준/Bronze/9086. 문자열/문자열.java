12c84ba3f6211130b8fbdd0bb480ca439e756b2a
Jaewon <60962150+JaewonnHan@users.noreply.github.com>
2024-09-10 12:40:01 +0900

    [Bronze V] Title: 문자열, Time: 96 ms, Memory: 14000 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -1,16 +1,17 @@
 import java.util.*;
 import java.io.*;
 public class Main{
-    public static void main(String[] args)throws IOException{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
-        
+        String[] str = new String[N];
         for(int i=0; i<N; i++){
-            String s= br.readLine();
-            sb.append(s.charAt(0));
-            sb.append(s.charAt(s.length()-1)+"\n");
+            String st = br.readLine();
+            str[i] = st.substring(0,1) + st.substring(st.length()-1, st.length());
         }
-        System.out.print(sb);
+        for(int i=0; i<N; i++){
+            System.out.println(str[i]);
+            
+        }
     }
 }
