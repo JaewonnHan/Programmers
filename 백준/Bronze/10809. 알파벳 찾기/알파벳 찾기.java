2d688777d05aa09e747992bd86e2ddcfa9d85f3f
Jaewon <60962150+JaewonnHan@users.noreply.github.com>
2024-09-11 17:01:00 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 116 ms, Memory: 15872 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,22 +1,22 @@
 import java.io.*;
+import java.util.*;
 public class Main{
     public static void main(String[] args)throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[26];
-        for(int i=0; i<26; i++){
-            arr[i] = -1;
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int[] array = new int[26];
+    for(int i=0; i<26; i++){
+    array[i] = -1;
+    }
+    String st = br.readLine();
+    for(int i=0; i<st.length(); i++){
+        char ch = st.charAt(i);
+        if(array[ch - 'a'] == -1){
+            array[ch-'a'] = i;
         }
-        String str = br.readLine();
-        for(int i=0; i<str.length(); i++){
-            char ch = str.charAt(i);
-            if(arr[ch -'a'] == -1){
-            arr[ch - 'a'] = i;
-        }
-        }
-        for(int a : arr){
-            System.out.print(a +" ");
-        }
-        
-        
+    }
+    for(int val : array){
+        System.out.print(val + " ");
+    
+       }
     }
 }
