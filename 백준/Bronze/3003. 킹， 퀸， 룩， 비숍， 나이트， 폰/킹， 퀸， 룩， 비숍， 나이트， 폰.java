7b8afc57368c85c2971694ea8587776109345258
Jaewon <60962150+JaewonnHan@users.noreply.github.com>
2024-09-15 14:27:47 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 112 ms, Memory: 15884 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -1,18 +1,31 @@
-import java.util.*;
 import java.io.*;
-
+import java.util.*;
 public class Main{
     public static void main(String[] args) throws IOException{
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        int[] chess = {1,1,2,2,2,8};
-        int[] number = new int[chess.length];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int k = 1;
+        int q = 1;
+        int r = 2;
+        int b = 2;
+        int n = 2;
+        int p = 8;
         
-        for(int i=0; i<chess.length; i++){
-            number[i] = Integer.parseInt(st.nextToken());
-        }
-        for(int i=0; i<chess.length; i++){
-            System.out.print(chess[i] - number[i]+" ");
-        }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        k = k - Integer.parseInt(st.nextToken());
+        q = q - Integer.parseInt(st.nextToken());
+        r = r - Integer.parseInt(st.nextToken());
+        b = b - Integer.parseInt(st.nextToken());
+        n = n - Integer.parseInt(st.nextToken());
+        p = p - Integer.parseInt(st.nextToken());
+        sb.append(k + " ");
+        sb.append(q + " ");
+        sb.append(r + " ");
+        sb.append(b + " ");
+        sb.append(n + " ");
+        sb.append(p + " ");
+        System.out.print(sb);
+        
+           
     }
 }
