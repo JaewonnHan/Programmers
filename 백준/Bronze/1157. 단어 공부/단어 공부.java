9b5cc41c9ceeae129c88894d26a6733a0e3af1ce
Jaewon <60962150+JaewonnHan@users.noreply.github.com>
2024-09-27 18:39:21 +0900

    [Bronze I] Title: 단어 공부, Time: 232 ms, Memory: 20116 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,27 +1,25 @@
-import java.io.*;
 import java.util.*;
-
+import java.io.*;
 public class Main{
-    public static void main(String[] args)throws IOException{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] arr = new int[26];
         String str = br.readLine();
         
         for(int i=0; i<str.length(); i++){
-            if('a'<= str.charAt(i) && str.charAt(i) <= 'z'){
-                arr[str.charAt(i)-97]++;
+            if('a' <= str.charAt(i) && str.charAt(i) <= 'z'){
+                arr[str.charAt(i) - 97]++;
             }else{
-                arr[str.charAt(i)-65]++;
+                arr[str.charAt(i) - 65]++;
             }
         }
-        
-        int max = -1;
+        int max = 0;
         char ch = '?';
         for(int i=0; i<26; i++){
             if(arr[i] > max){
                 max = arr[i];
                 ch = (char)(i+65);
-            }else if(arr[i] == max){
+            }else if (arr[i] == max){
                 ch = '?';
             }
         }
